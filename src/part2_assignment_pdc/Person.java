59d288023d46eb9a83b2c85e97168cf08189a2a0
anikakanizesha <105477189+anikakanizesha@users.noreply.github.com>
2023-06-03 02:21:11 +1200

    This is the programming assignment

--- src/part2_assignment_pdc/Person.java
+++ src/part2_assignment_pdc/Person.java
@@ -8,6 +8,39 @@ package part2_assignment_pdc;
  *
  * @author anika
  */
-public class Person {
+public class Person  {
+    
+    private String name;
+    private int customerAge;
+    private String customerAddress;
+    
+    public Person(){
+    }
+    
+    public Person(String name, int customer_age, String customer_address)
+    {
+        this.name = name;
+        this.customerAge = customer_age;
+        this.customerAddress = customer_address;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    public int getCustomerAge(){
+        return this.customerAge;
+        
+    }
+    public void setCustomerAge(int customerAge){
+        this.customerAge = customerAge;
+    }
+    
+    
+    
     
 }
+
