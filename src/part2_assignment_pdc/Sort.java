9be9fa6045ff1a42ea56d09ed7a8556c55592712
anikakanizesha <105477189+anikakanizesha@users.noreply.github.com>
2023-06-09 01:13:16 +1200

    This is the programming assignment

--- src/part2_assignment_pdc/Sort.java
+++ src/part2_assignment_pdc/Sort.java
@@ -8,6 +8,51 @@ package part2_assignment_pdc;
  *
  * @author anika
  */
-public class Sort {
+public class Sort extends Reservation{
+    private final double BASIC_PAYMENT = 400;
+    private Room[] rooms;
+    private int numberOfRooms = 0;
+
+    public Sort(){
+        rooms = new Room[3];
+    }
+
+    public Sort(String name, int age, String address){
+        super(name, age, address);
+        rooms = new Room[3];
+    }
+
+    public void addRoom(Room newRoom) {
+        if(numberOfRooms < rooms.length){
+            rooms[numberOfRooms] = newRoom;
+            numberOfRooms++;
+        }else{
+            System.out.println("You have crossed room booking limit\n");
+        }
+    }
+
+    public void printRooms() {
+        for (int i = 0; i < numberOfRooms; i++) {
+            System.out.print(rooms[i].toString());
+        }
+        System.out.println();
+    }
+
+    public int getNumberOfRooms() {
+        return this.numberOfRooms;
+    }
+
+    public double getPayment() {
+        double rent = BASIC_PAYMENT + (numberOfRooms)*(BASIC_PAYMENT*0.20);
+        return rent;
+    }
+
+    
+    @Override
+    public String toString(){
+        return  super.toString() +
+                "Rent: " + getPayment() + "\n";
+    }
     
 }
+
