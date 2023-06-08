e6b16bedcf79872223db3b4ed0f1bf26d6dfc208
anikakanizesha <105477189+anikakanizesha@users.noreply.github.com>
2023-06-08 16:29:34 +1200

    This is the programming assignment

--- src/part2_assignment_pdc/Customer.java
+++ src/part2_assignment_pdc/Customer.java
@@ -8,6 +8,78 @@ package part2_assignment_pdc;
  *
  * @author anika
  */
-public class Customer {
-    
+public class Customer extends Person{
+    private double daysStaying;
+    private String reservationCode;
+    private Room[] rooms;
+    private int numberOfRooms = 0;
+
+    public Customer(){
+        rooms = new Room[6];
+    }
+
+    public Customer(String name, int customerAge, String customerAddress, double daysStaying, String reservationCode){
+        super(name, customerAge, customerAddress);
+        this.daysStaying = daysStaying;
+        this.reservationCode = reservationCode;
+        if (daysStaying<2) {
+            rooms = new Room[4];
+        }else{
+            rooms = new Room[6];
+        }
+    }
+
+
+    public double getCgpa() {
+        return this.cgpa;
+    }
+
+    public void setCgpa(double cgpa) {
+        this.cgpa = cgpa;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Course[] getCourses() {
+        return this.courses;
+    }
+
+    public int getNumberOfCourses() {
+        return this.numberOfCourses;
+    }
+
+    public void addCourse(Course newCourse){
+        if(numberOfCourses<courses.length){
+            courses[numberOfCourses] = newCourse;
+            numberOfCourses++;
+        }else{
+            System.out.println("You are not allowed to take more than " + courses.length + " courses \n");
+        }
+        
+    }
+
+    public void printCourses(){
+        for (int i = 0; i < numberOfCourses; i++) {
+             System.out.print(courses[i].toString());
+        }
+        System.out.println();
+    }
+
+
+
+
+    @Override
+    public String toString(){
+        return super.toString() + 
+                "cgpa: " + getCgpa() + "\n" +
+                "id: " + getId() + "\n";
+    }
+
 }
+
