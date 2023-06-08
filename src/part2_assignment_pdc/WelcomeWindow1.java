d84705944674513a2e4595740f98bce41d3f715f
anikakanizesha <105477189+anikakanizesha@users.noreply.github.com>
2023-06-09 04:01:36 +1200

    This is the programming assignment

--- src/part2_assignment_pdc/WelcomeWindow1.java
+++ src/part2_assignment_pdc/WelcomeWindow1.java
@@ -4,11 +4,114 @@
  */
 package part2_assignment_pdc;
 
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JToolBar;
+import java.awt.BorderLayout;
+import java.awt.SystemColor;
+import java.awt.Color;
+import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
+import javax.swing.JTextArea;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 /**
  *
  * @author anika
  */
 public class WelcomeWindow1 {
-    //Hi how are you
-    
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					WelcomeWindow1 window = new WelcomeWindow1();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public WelcomeWindow1() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(new Color(119, 136, 153));
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
+		panel.setForeground(new Color(75, 0, 130));
+		panel.setBackground(new Color(250, 250, 210));
+		panel.setBounds(41, 36, 539, 72);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Welcome to Online Portal");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 25));
+		lblNewLabel.setBounds(109, 10, 338, 52);
+		panel.add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("Admin Login");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.dispose();
+				AdminLogin ad = new AdminLogin();
+				ad.setVisible(true);
+				
+			}
+		});
+		btnNewButton.setBackground(SystemColor.menu);
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 20));
+		btnNewButton.setBounds(164, 147, 270, 62);
+		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Student Login");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.dispose();
+				CustomerLogin st = new CustomerLogin();
+				st.setVisible(true);
+			}
+		});
+		btnNewButton_1.setBackground(SystemColor.menu);
+		btnNewButton_1.setFont(new Font("Tahoma", Font.BOLD, 20));
+		btnNewButton_1.setBounds(164, 219, 270, 55);
+		frame.getContentPane().add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Faculty Login");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.dispose();
+				ReservationLogin ft = new ReservationLogin();
+				ft.setVisible(true);
+			}
+		});
+		btnNewButton_2.setBackground(SystemColor.menu);
+		btnNewButton_2.setFont(new Font("Tahoma", Font.BOLD, 20));
+		btnNewButton_2.setBounds(164, 284, 270, 62);
+		frame.getContentPane().add(btnNewButton_2);
+		frame.setBounds(100, 100, 649, 455);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 }
