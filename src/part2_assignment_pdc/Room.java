fc2c14eea681d9d23208ffbde50d91df657c9b16
anikakanizesha <105477189+anikakanizesha@users.noreply.github.com>
2023-06-09 21:37:33 +1200

    This is the programming assignment

--- src/part2_assignment_pdc/Room.java
+++ src/part2_assignment_pdc/Room.java
@@ -10,37 +10,38 @@ package part2_assignment_pdc;
  */
 public class Room {
     
-    private String nameOfFloor;
-    private String roomNumber;
+    private String nameOfFloor; // Name of the floor where the room is located
+    private String roomNumber; // Room number
     
     public Room() {
+        // Default constructor
     }
 
     public Room(String nameOfFloor, String roomNumber) {
-        this.nameOfFloor = nameOfFloor;
-        this.roomNumber = roomNumber;
+        this.nameOfFloor = nameOfFloor; // Initialize the name of the floor
+        this.roomNumber = roomNumber; // Initialize the room number
     }
 
     public String getNameOfFloor() {
-        return this.nameOfFloor;
+        return this.nameOfFloor; // Retrieve the name of the floor
     }
 
     public void setNameOfFloor(String nameOfFloor) {
-        this.nameOfFloor = nameOfFloor;
+        this.nameOfFloor = nameOfFloor; // Set the name of the floor
     }
 
     public String getRoomNumber() {
-        return this.roomNumber;
+        return this.roomNumber; // Retrieve the room number
     }
 
     public void setRoomNumber(String roomNumber) {
-        this.roomNumber = roomNumber;
+        this.roomNumber = roomNumber; // Set the room number
     }
 
     @Override
     public String toString() {
         return "Name: " + getNameOfFloor() + "\n" +
-               "Course Code: " + getRoomNumber() + "\n";
+               "Room number: " + getRoomNumber() + "\n"; // Return a string representation of the room object
     }
 
 }
