58c6668d9aee7cf32cb82a47af357cf3fbe198e3
anikakanizesha <105477189+anikakanizesha@users.noreply.github.com>
2023-06-09 22:20:54 +1200

    This is the programming assignment

--- src/part2_assignment_pdc/CustomerLogin.java
+++ src/part2_assignment_pdc/CustomerLogin.java
@@ -8,6 +8,137 @@ package part2_assignment_pdc;
  *
  * @author anika
  */
-public class CustomerLogin {
-    
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import java.awt.SystemColor;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class CustomerLogin extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textFieldUsername;
+	private JTextField textFieldPassword;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CustomerLogin frame = new CustomerLogin();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public CustomerLogin() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 690, 444);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 204, 204));
+		contentPane.setBorder(new LineBorder(new Color(0, 0, 0), 3, true));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new LineBorder(new Color(0, 0, 0), 3, true));
+		panel.setBackground(new Color(230, 230, 250));
+		panel.setBounds(38, 10, 628, 62);
+		contentPane.add(panel);
+		
+		JLabel lblNewLabel = new JLabel("Customer Login ");
+		lblNewLabel.setBackground(new Color(240, 240, 240));
+		lblNewLabel.setForeground(new Color(25, 25, 112));
+		lblNewLabel.setFont(new Font("Arial", Font.BOLD, 28));
+		panel.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Username");
+		lblNewLabel_1.setFont(new Font("Arial", Font.PLAIN, 18));
+		lblNewLabel_1.setBounds(71, 130, 81, 13);
+		contentPane.add(lblNewLabel_1);
+		
+		textFieldUsername = new JTextField();
+		textFieldUsername.setBounds(162, 113, 423, 51);
+		contentPane.add(textFieldUsername);
+		textFieldUsername.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("Password");
+		lblNewLabel_2.setFont(new Font("Arial", Font.PLAIN, 18));
+		lblNewLabel_2.setBounds(71, 194, 81, 19);
+		contentPane.add(lblNewLabel_2);
+		
+		textFieldPassword = new JTextField();
+		textFieldPassword.setBounds(162, 194, 423, 51);
+		contentPane.add(textFieldPassword);
+		textFieldPassword.setColumns(10);
+		
+		JButton btnNewButton = new JButton("LOGIN");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String password = textFieldPassword.getText();
+				String username = textFieldUsername.getText();
+				if(password.contains("Login")&& username.contains("Student")){
+					textFieldPassword.setText(null);
+					textFieldUsername.setText(null);
+					
+				}
+				else
+				{
+					JOptionPane.showMessageDialog(null, "Invalid Login Details","Login Error",JOptionPane.ERROR_MESSAGE);
+					textFieldPassword.setText(null);
+					textFieldUsername.setText(null);
+				}
+			}
+		});
+		btnNewButton.setFont(new Font("Arial", Font.BOLD, 15));
+		btnNewButton.setBackground(new Color(250, 240, 230));
+		btnNewButton.setBounds(67, 286, 100, 40);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("RESET");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textFieldUsername.setText(null);
+				textFieldPassword.setText(null);
+			}
+		});
+		btnNewButton_1.setBackground(new Color(250, 240, 230));
+		btnNewButton_1.setFont(new Font("Arial", Font.BOLD, 15));
+		btnNewButton_1.setBounds(279, 286, 100, 40);
+		contentPane.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("EXIT");
+		btnNewButton_2.setBackground(new Color(250, 240, 230));
+		btnNewButton_2.setFont(new Font("Arial", Font.BOLD, 15));
+		btnNewButton_2.setBounds(500, 286, 100, 40);
+		contentPane.add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("Go Back To Welcome Page");
+		btnNewButton_3.setBackground(new Color(250, 240, 230));
+		btnNewButton_3.setFont(new Font("Arial", Font.BOLD, 18));
+		btnNewButton_3.setBounds(117, 345, 433, 40);
+		contentPane.add(btnNewButton_3);
+	}
+
 }
